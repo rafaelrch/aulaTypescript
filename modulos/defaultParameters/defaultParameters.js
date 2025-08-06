2287036de3a559f63c5f50123df3f4676b84e9e3
Rafael Almeida Rocha <rafaelrocha250304@gmail.com>
2025-08-06 20:59:53 -0300

    Aplicando default parameters

---
+++ modulos/defaultParameters/defaultParameters.js
@@ -0,0 +1,22 @@
+"use strict";
+// 1
+function descontoCompra(preco, desconto = 0.08) {
+    return preco * (1 - desconto);
+}
+;
+console.log(descontoCompra(100));
+// 2
+function exibirMensagem(mensagem, saudar = 'Fala, pessoal!') {
+    return saudar + ' ' + mensagem + '!';
+}
+console.log(exibirMensagem('JavaScript Developers'));
+// 3
+function exibirNome(nome, sobrenome = 'Rocha') {
+    return nome + ' ' + sobrenome;
+}
+const resultado_1 = exibirNome('Rafael');
+const resultado_2 = exibirNome('Glaucia', undefined);
+const resultado_3 = exibirNome('Rafael', 'Almeida Rocha');
+console.log(resultado_1);
+console.log(resultado_2);
+console.log(resultado_3);
