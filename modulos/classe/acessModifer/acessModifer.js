0a45af0c7c9fdd4c6d5b191a468a1cfdc966954d
Rafael Almeida Rocha <rafaelrocha250304@gmail.com>
2025-08-09 22:34:36 -0300

    acess modifer public

---
+++ modulos/classe/acessModifer/acessModifer.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// 1 Public
+class Estudante {
+}
+const estudante = new Estudante();
+estudante.codigoEstudante = 123;
+estudante.nomeEstudante = "Rafael";
+console.log(estudante.codigoEstudante);
+console.log(estudante.nomeEstudante);
+// 2 Private
+// 3 Private
+// 4 Protected
+// 5 Readonly
---
+++ modulos/classe/acessModifer/acessModifer.ts
@@ -0,0 +1,25 @@
+export {}
+
+// 1 Public
+
+class Estudante {
+    codigoEstudante: number;
+    nomeEstudante: string
+}
+
+const estudante = new Estudante();
+estudante.codigoEstudante = 123;
+estudante.nomeEstudante = "Rafael";
+
+console.log(estudante.codigoEstudante);
+console.log(estudante.nomeEstudante);
+
+// 2 Private
+
+// 3 Private
+
+// 4 Protected
+
+// 5 Readonly
+
+
