4bdfdf5a45bd6a3837d37066c21f2bf176843ce0
Rafael Almeida Rocha <rafaelrocha250304@gmail.com>
2025-08-12 21:32:34 -0300

    get e set denso

--- modulos/classe/getSet/getSet.js
+++ modulos/classe/getSet/getSet.js
@@ -21,3 +21,33 @@ const pessoa = new Pessoa();
 pessoa.retornarNomePessoa('Rocha');
 console.log('Meu nome é...: ', pessoa.nome);
 // Get mais denso
+class Estudante {
+    constructor() {
+        this._nome = 'Rafael Almeida';
+    }
+    get nomeEstudante() {
+        return this._nome;
+    }
+}
+const estudante = new Estudante();
+const resultado = estudante.nomeEstudante;
+console.log('O nome do estudante é ', resultado);
+// Set/Get mais denso
+class Estudante_02 {
+    /**
+     *
+     */
+    constructor(nome, semestre, curso) {
+        this.nome = nome;
+        this.semeste = semestre;
+        this.curso = curso;
+    }
+    get cursos() {
+        return this.curso;
+    }
+}
+const estudante_02 = new Estudante_02('Rocha Rafael', 8, 'Analise e desenvolvimento de sistemas');
+console.log(estudante_02);
+estudante_02.curso = 'Engenharia de Software';
+console.log('Curso atualizado: ', estudante_02.curso);
+console.log(estudante_02);
--- modulos/classe/getSet/getSet.ts
+++ modulos/classe/getSet/getSet.ts
@@ -28,3 +28,46 @@ pessoa.retornarNomePessoa('Rocha');
 console.log('Meu nome é...: ', pessoa.nome)
 
 // Get mais denso
+
+class Estudante {
+    private _nome = 'Rafael Almeida';
+    private _semestre: number;
+    private _curso: string;
+
+    public get nomeEstudante() {
+        return this._nome;
+    }
+}
+
+const estudante = new Estudante();
+const resultado = estudante.nomeEstudante;
+console.log('O nome do estudante é ', resultado)
+
+// Set/Get mais denso
+
+class Estudante_02 {
+    nome: string;
+    semeste: number;
+    curso: string;
+
+    /**
+     *
+     */
+    constructor(nome: string, semestre: number, curso:string) {
+        this.nome = nome;
+        this.semeste = semestre;
+        this.curso = curso      
+    }
+
+    public get cursos(){
+        return this.curso
+    }
+
+}
+
+const estudante_02 = new Estudante_02('Rocha Rafael', 8, 'Analise e desenvolvimento de sistemas');
+console.log(estudante_02);
+
+estudante_02.curso = 'Engenharia de Software';
+console.log('Curso atualizado: ', estudante_02.curso);
+console.log(estudante_02);
