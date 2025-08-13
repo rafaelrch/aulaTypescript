fc05920b94b6eed2c87a93d60ec835f2697ee137
Rafael Almeida Rocha <rafaelrocha250304@gmail.com>
2025-08-12 22:10:27 -0300

    exercicios de classe 2

--- modulos/classe/exercicios.js
+++ modulos/classe/exercicios.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// 1
 class Produto {
     /**
      *
@@ -29,3 +30,39 @@ const produto = new Produto(30);
 console.log('Preço atual:', produto.exibirInfo());
 produto.setPreco = 50;
 console.log('Preço atualizado:', produto.exibirInfo());
+// 2
+class Funcionario {
+    constructor(nome, cargo, salario) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+    get getSalario() {
+        return this.salario;
+    }
+    promover(novoCargo, aumento) {
+        this.cargo = novoCargo;
+        this.salario = aumento;
+    }
+    exibirDados() {
+        return `Nome: ${this.nome}. 
+        Cargo: ${this.cargo}.
+        Salario: R$${this.salario}`;
+    }
+}
+const funcionario = new Funcionario('Pedro', 'Desenvolvedor Jr', 2000);
+console.log('Dados atual funcionario : ', funcionario.exibirDados());
+funcionario.promover('Desenvolvedor Pleno', 500);
+console.log('Dados do funcionario atualizado: ', funcionario.exibirDados());
+class Gerente extends Funcionario {
+    constructor(nome, cargo, salario) {
+        super(nome, cargo, salario);
+    }
+    exibirDadosGerente() {
+        return `Nome: ${this.nome}. 
+        Cargo: ${this.cargo}`;
+    }
+}
+const gerente = new Gerente('Mateus', 'RH', 2900);
+console.log('Dados do atual do gerente: ', gerente.exibirDadosGerente());
+// 3
--- modulos/classe/exercicios.ts
+++ modulos/classe/exercicios.ts
@@ -1,5 +1,8 @@
 export {}
 
+
+// 1
+
 class Produto{
     private _preco: number;
 
@@ -32,4 +35,67 @@ const produto = new Produto(30);
 console.log('Preço atual:', produto.exibirInfo())
 produto.setPreco = 50;
 console.log('Preço atualizado:', produto.exibirInfo())
+
+// 2
+
+class Funcionario{
+    nome: string;
+    protected cargo: string;
+    private salario: number;
+
+    constructor(nome: string, cargo: string, salario: number) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public get getSalario(){
+        return this.salario
+    }
+
+    promover(novoCargo: string, aumento: number){
+        this.cargo = novoCargo;
+        this.salario = aumento;
+    }
+
+    exibirDados(){
+        return `Nome: ${this.nome}. 
+        Cargo: ${this.cargo}.
+        Salario: R$${this.salario}`
+    }
+}
+
+const funcionario = new Funcionario('Pedro', 'Desenvolvedor Jr', 2000);
+console.log(
+    'Dados atual funcionario : ', 
+    funcionario.exibirDados()
+    
+)
+
+funcionario.promover('Desenvolvedor Pleno', 500)
+console.log(
+    'Dados do funcionario atualizado: ', funcionario.exibirDados()
+    
+)
+
+
+class Gerente extends Funcionario {
+    
+    constructor(nome: string, cargo: string, salario: number) {
+        super(nome, cargo, salario)
+    }
+
+    exibirDadosGerente(){
+        return `Nome: ${this.nome}. 
+        Cargo: ${this.cargo}`
+    }
+}
+
+const gerente = new Gerente('Mateus', 'RH', 2900)
+console.log(
+    'Dados do atual do gerente: ', gerente.exibirDadosGerente()
+    
+)
+
+// 3
 
