aeddc709ced4416215e90dda00dee6b944fff8d5
Rafael Almeida Rocha <rafaelrocha250304@gmail.com>
2025-08-13 21:59:19 -0300

    questao 2 exercicio 2

--- modulos/classe/exercicios2.js
+++ modulos/classe/exercicios2.js
@@ -8,22 +8,76 @@ class ContaBancaria {
     get saldo() {
         return this._saldo;
     }
+    set saldo(valor) {
+        if (valor !== this._saldo) {
+            this._saldo = valor;
+            console.log(`Saldo atualizado: R$${this._saldo}`);
+        }
+    }
     depositar(valor) {
-        return this._saldo += valor;
+        if (valor <= 0) {
+            console.log("Deposito inválido");
+            return false;
+        }
+        this.saldo = this._saldo + valor;
+        return true;
     }
     sacar(valor) {
         if (valor > this._saldo) {
-            return 'Valor insuficiente';
+            console.log('Valor insuficiente');
+            return false;
         }
         else {
-            return this._saldo -= valor;
+            this.saldo = this._saldo - valor;
+            return true;
         }
     }
 }
-const conta = new ContaBancaria('Rafael', 2000);
+const conta = new ContaBancaria('Rafael', 3000);
 console.log('Saldo: ', conta.saldo);
 conta.depositar(500);
 conta.sacar(200);
 console.log('Novo saldo: ', conta.saldo);
-conta.sacar(2500);
+conta.sacar(3000);
 console.log('Novo saldo: ', conta.saldo);
+// 2
+console.log("----------------------");
+class Carro {
+    constructor(modelo, _velocidade) {
+        this._velocidade = _velocidade;
+        this.modelo = modelo;
+    }
+    get velocidade() {
+        return this._velocidade;
+    }
+    set velocidade(setVelocidade) {
+        if (setVelocidade >= 200) {
+            console.log('Vc atingiu a velocidade maxima de 200km/h');
+        }
+        else {
+            this._velocidade += setVelocidade;
+        }
+    }
+    acelerar(valor) {
+        if (valor <= 0) {
+            console.log("Valor de aceleracao invalido");
+        }
+        this.velocidade = this._velocidade + valor;
+    }
+    frear(valor) {
+        if (valor <= 0) {
+            console.log("Valor de feragem invalido");
+        }
+        this.velocidade = this._velocidade - valor;
+    }
+}
+const carro = new Carro('Ferrari', 100);
+console.log("Velocidade atual: ", carro.velocidade, "km/h");
+console.log("Acelerando... ");
+carro.acelerar(100);
+console.log("Freando... ");
+carro.frear(20);
+console.log("Velocidade atual: ", carro.velocidade, "km/h");
+console.log("Acelerando... ");
+carro.velocidade = 20;
+console.log("Velocidade atual:", carro.velocidade, "km/h");
--- modulos/classe/exercicios2.ts
+++ modulos/classe/exercicios2.ts
@@ -14,20 +14,35 @@ class ContaBancaria {
         return this._saldo;
     }
 
-    depositar(valor: number){
-        return this._saldo += valor;
+    private set saldo(valor: number){
+        if(valor !== this._saldo){
+            this._saldo = valor;
+            console.log(`Saldo atualizado: R$${this._saldo}`)
+        }
     }
 
-    sacar(valor: number){
+    depositar(valor: number): boolean{
+        if(valor <= 0){
+            console.log("Deposito inválido")
+            return false
+        }
+
+        this.saldo = this._saldo + valor;
+        return true
+    }
+
+    sacar(valor: number): boolean{
         if(valor > this._saldo ){
-            return 'Valor insuficiente'
+            console.log('Valor insuficiente')
+            return false
         }else{
-            return this._saldo -= valor
+            this.saldo = this._saldo - valor;
+            return true
         }
     }
 }
 
-const conta = new ContaBancaria('Rafael', 2000);
+const conta = new ContaBancaria('Rafael', 3000);
 console.log('Saldo: ', conta.saldo);
 
 conta.depositar(500);
@@ -35,5 +50,65 @@ conta.sacar(200)
 
 console.log('Novo saldo: ', conta.saldo);
 
-conta.sacar(2500)
+conta.sacar(3000)
 console.log('Novo saldo: ', conta.saldo);
+
+// 2
+
+console.log("----------------------")
+
+class Carro{
+    public modelo: string;
+    private _velocidade: number;
+
+
+    constructor(modelo: string, _velocidade: number) {
+        this._velocidade =_velocidade;
+        this.modelo = modelo;
+        
+    }
+
+    public get velocidade(){
+        return this._velocidade;
+    }
+
+    public set velocidade(setVelocidade: number){
+        if(setVelocidade >= 200){
+            console.log('Vc atingiu a velocidade maxima de 200km/h')
+        }else {
+            this._velocidade += setVelocidade;
+        }
+    }
+
+    acelerar(valor: number){
+
+        if(valor <= 0){
+            console.log("Valor de aceleracao invalido")
+        }
+        this.velocidade = this._velocidade + valor;
+
+    }
+
+    frear(valor: number){
+        if(valor <= 0){
+            console.log("Valor de feragem invalido")
+        }
+        this.velocidade = this._velocidade - valor;
+    }
+}
+
+const carro = new Carro('Ferrari', 100);
+console.log("Velocidade atual: ", carro.velocidade,"km/h")
+
+console.log("Acelerando... ")
+carro.acelerar(100);
+
+console.log("Freando... ")
+carro.frear(20);
+console.log("Velocidade atual: ", carro.velocidade,"km/h")
+
+console.log("Acelerando... ")
+carro.velocidade = 20;
+console.log("Velocidade atual:", carro.velocidade, "km/h")
+
+
