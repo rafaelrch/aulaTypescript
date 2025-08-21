dd36414d76893c3a8f37e617fb538655e2cdfb9b
Rafael Almeida Rocha <rafaelrocha250304@gmail.com>
2025-08-21 20:43:00 -0300

    Adicionando opcao de excluir tarefa

--- projetos/toDo/todo.ts
+++ projetos/toDo/todo.ts
@@ -18,6 +18,7 @@ function mostrarMenu(){
     console.log("1 - Ver Tarefas");
     console.log("2 - Adicionar Tarefa");
     console.log("3 - Editar Tarefa");
+    console.log("4 - Excluir Tarefa");
     console.log("0 - Sair");
     
     rl.question("O que vc deseja fazer? ", (resposta) => {
@@ -40,6 +41,10 @@ function tratarOpcao(opcao: number){
 
         case 3:
             editar();
+        break;
+
+        case 4:
+            excluir();
         break;
 
         case 0:
@@ -67,6 +72,8 @@ function listarTarefas(){
 }
 
 function adicionar(){
+    
+
     rl.question("Digite o nome da tarefa: ", (resposta) => {
         const task = resposta
         if(!task){
@@ -81,18 +88,56 @@ function adicionar(){
 }
 
 function editar(){
+
+    if(todoTarefas.length === 0){
+        return mostrarMenu()
+    }
+
+    console.log("\nTarefas");
+    todoTarefas.forEach((t, i) => console.log(`${i+1} - ${t.nome}`))
+
     rl.question("Qual tarefa vc quer editar? ", (resposta) => {
         const numTask = Number(resposta)
-        if(numTask <= 0){
-            console.log("Invalido")
-        }else{
-            rl.question("Digite a alteracao: ", (resposta2) => {
-                const taskEditada = resposta2;
-                todoTarefas[numTask] = {nome: taskEditada, status: false};
-            })
-
+        const idx = numTask - 1;
+        if(!Number.isInteger(numTask) || numTask <= 0 || numTask >= todoTarefas.length){
+            console.log("Invalido");
+            return mostrarMenu()
         }
+        
+        rl.question("Digite a alteracao(nome): ", (resposta2) => {
+            if(!resposta2){
+                console.log("Tarefa Invalida")
+                return mostrarMenu()
+            }else{
+                const atual = todoTarefas[idx];
+                todoTarefas[idx] = {...atual, nome: resposta2};
+            }
+
+            console.log("Tarefa editada com sucesso!");
+            mostrarMenu();
+        })
+    
     });
+}
+
+function excluir(){
+    
+    console.log("--------Todo---------")
+    todoTarefas.forEach((t, i) => console.log(`${i+1} - ${t.nome}`))
+
+    rl.question("Qual tarefa vc quer excluir?", (resposta) => {
+        const numExcluir = Number(resposta);
+        const idx = numExcluir - 1
+        if(numExcluir <= 0 || numExcluir > todoTarefas.length){
+            console.log("Escolha inválida")
+            return mostrarMenu()
+        }else{
+            todoTarefas.splice(idx, 1)
+            console.log("Tarefa removida com sucesso")
+            return mostrarMenu()
+        }
+
+    })
 }
 
 mostrarMenu()
